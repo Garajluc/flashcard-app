080b639afde5799c276346f87860a4c945ec2c5a
garajluc <lucia.garajova.upa@gmail.com>
2023-06-19 14:37:01 +0200

    add tests

--- src/api/collection/CollectionService.test.ts
+++ src/api/collection/CollectionService.test.ts
@@ -1,10 +1,14 @@
-import { createOrUpdateCollections } from './CollectionService';
-import type { CollectionRequestBody, Collections } from '@/data/types';
+import {
+  createOrUpdateCollections,
+  getCollectionById,
+  updateCollections,
+} from './CollectionService';
+import type { Collection, CollectionsWithId } from '@/data/types';
 
 describe('CollectionService', () => {
   describe('createOrUpdateCollections', () => {
     it('should add new flashcard into existing collection', () => {
-      const collections: Collections = [
+      const collections: CollectionsWithId = [
         {
           id: '1',
           category_id: 'existing_category',
@@ -35,7 +39,7 @@ describe('CollectionService', () => {
     });
 
     it('should create new collection if not matching category_name found', () => {
-      const collections: Collections = [
+      const collections: CollectionsWithId = [
         {
           id: '1',
           category_id: 'existing_category',
@@ -67,7 +71,7 @@ describe('CollectionService', () => {
     });
 
     it('should throw error if missing data in the POST request', () => {
-      const collections: Collections = [
+      const collections: CollectionsWithId = [
         {
           id: '1',
           category_id: 'existing_category',
@@ -87,11 +91,60 @@ describe('CollectionService', () => {
       };
 
       expect(() =>
-        createOrUpdateCollections(
-          collections,
-          requestBody as CollectionRequestBody
-        )
+        createOrUpdateCollections(collections, requestBody as Collection)
       ).toThrow();
+    });
+  });
+
+  describe('getCollectionById', () => {
+    it('should return collection if matching id found', () => {
+      const collections: CollectionsWithId = [
+        {
+          id: '1',
+          category_id: 'existing_category',
+          category_name: 'Existing Category',
+          flashcards: [],
+        },
+      ];
+
+      const collection = getCollectionById('1', collections);
+
+      expect(collection).toBeTruthy();
+    });
+  });
+
+  describe('updateCollections', () => {
+    it('should update collections', () => {
+      const collections: CollectionsWithId = [
+        {
+          id: '1',
+          category_id: 'existing_category',
+          category_name: 'Existing Category',
+          flashcards: [],
+        },
+      ];
+
+      const requestBody = {
+        id: '1',
+        category_id: 'existing_category',
+        category_name: 'Existing Category',
+        flashcards: [
+          {
+            question: 'New Question',
+            answer: 'New Answer',
+          },
+        ],
+      };
+
+      const updatedCollections = updateCollections(
+        collections,
+        requestBody.id,
+        requestBody
+      );
+
+      expect(updatedCollections).toHaveLength(1);
+      expect(updatedCollections[0].category_id).toEqual('existing_category');
+      expect(updatedCollections[0].flashcards).toHaveLength(1);
     });
   });
 });
