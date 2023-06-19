b5cc0b33f3a6c1d24efa236aea194c65da8f7811
garajluc <lucia.garajova.upa@gmail.com>
2023-06-19 16:33:26 +0200

    quiz end page ui

--- src/components/collectionQuiz/CollectionQuizEnd.tsx
+++ src/components/collectionQuiz/CollectionQuizEnd.tsx
@@ -1,4 +1,5 @@
-import { Card, Grid } from '@mui/material';
+import Image from 'next/image';
+import { Card, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 import { WithTooltip } from '../utils/WithTooltip';
 import { FLIP_CARD_HEIGHT, FLIP_CARD_MIN_HEIGHT } from './flipCard/FlipCard';
@@ -6,8 +7,8 @@ import { FLIP_CARD_COUNTER_TOTAL_HEIGHT } from './flipCard/FlipCardCounter';
 
 export const CollectionQuizEnd = () => {
   return (
-    <Link href="/">
-      <WithTooltip title={'Click to go back'} arrow={false}>
+    <WithTooltip title={'Click to go back'} arrow={false}>
+      <Link href="/" legacyBehavior>
         <Card
           sx={{
             position: 'relative',
@@ -15,22 +16,27 @@ export const CollectionQuizEnd = () => {
             height: FLIP_CARD_HEIGHT,
             minHeight: FLIP_CARD_MIN_HEIGHT,
             '&:hover': {
+              cursor: 'pointer',
               boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
             },
           }}
         >
-          <Grid
-            container
+          <Stack
             justifyContent="center"
             alignItems="center"
-            sx={{
-              height: '100%',
-            }}
+            sx={{ height: '100%' }}
           >
-            Congratulations!
-          </Grid>
+            <Image
+              src="/congrats.png"
+              priority
+              width={335}
+              height={250}
+              alt="Quiz finished"
+            />
+            <Typography variant="h2">Congratulations!</Typography>
+          </Stack>
         </Card>
-      </WithTooltip>
-    </Link>
+      </Link>
+    </WithTooltip>
   );
 };
